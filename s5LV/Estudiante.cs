d6cfbdf585c57f58f884382ca225a7c0f255d8ab
GrayBismuthMaster <73077314+GrayBismuthMaster@users.noreply.github.com>
2023-07-04 00:25:27 -0500

    Add files via upload

---
+++ s5LV/Estudiante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace s5LV
+{
+    internal class Estudiante
+    {
+        public int codigo { set; get; }
+        public string nombre { set; get; }
+        public string apellido { set; get; }
+        public int edad { set; get; }
+    }
+}
+
+
